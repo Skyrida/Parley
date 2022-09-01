9c78965dd5844afb834e3632f15380aa929b6ec3
Dennis Franz <dtf.87@freenet.de>
2022-09-01 17:59:37 +0800

    search

---
+++ app/javascript/controllers/searchbar.js
@@ -0,0 +1,21 @@
+<script>
+function searchFunction() {
+
+
+  input = debate.getElementById('searchquery');
+
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTitle("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    }
+
+    else {
+      li[i].style.display = "none";
+    }
+  }
+}
+</script>
--- app/models/debate.rb
+++ app/models/debate.rb
@@ -3,6 +3,7 @@ class Debate < ApplicationRecord
   has_many :chatrooms
   has_many :arguments
 
+
   validates :title, presence: true, uniqueness: true, length: { in: 2..100 }
   validates :description, length: { maximum: 300 }
   acts_as_taggable_on :tags
