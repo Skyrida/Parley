3da3343459a24786a149f35a7b142bb9d6b183af
Alex Rich <alexanderrich28@gmail.com>
2022-09-01 18:00:05 +0800

    Feat: Created Debates, Arguments and updated Swiper

--- app/controllers/debates_controller.rb
+++ app/controllers/debates_controller.rb
@@ -29,8 +29,13 @@ class DebatesController < ApplicationController
 
   def create
     @debate = Debate.new(debate_params)
-    @debate.save
-    redirect_to debate_path(@debate)
+    @debate.user = current_user
+
+    if @debate.save
+      redirect_to debate_path(@debate)
+    else
+      redirect_to new_debate_path
+    end
   end
 
   def destroy
@@ -40,6 +45,6 @@ class DebatesController < ApplicationController
   private
 
   def debate_params
-    params.require(:debate).permit(:title, :image)
+    params.require(:debate).permit(:title, :description, :tag_list)
   end
 end
