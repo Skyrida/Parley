3f8b875ed9cf983bc5f68d72e164a22fc0f53e5d
Dennis Franz <dtf.87@freenet.de>
2022-09-05 17:25:57 +0800

    idothings

---
+++ app/javascript/controllers/userdebatesearch_controller.js
@@ -0,0 +1,38 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="userdebatesearch"
+export default class extends Controller {
+
+  static targets = [ "searchbarform", "debatesCards", "formQuery"]
+
+  appear() {
+    this.userdebatesearchformTarget.classList.toggle("d-none")
+  }
+
+  searchFunction(event) {
+      console.log("in search")
+      const url = `${this.formQueryTarget.action}?title=${event.target.value}`
+      event.preventDefault()
+      fetch(url, {
+        headers: { "Accept": "application/json" },
+      })
+        .then(response => response.json())
+        .then((data) => {
+          this.debatesCardsTarget.innerHTML = (data.debates)
+        })
+    // input = debate.getElementById('searchquery');
+
+
+    // // Loop through all list items, and hide those who don't match the search query
+    // for (i = 0; i < li.length; i++) {
+    //   a = li[i].getElementsByTitle("a")[0];
+    //   txtValue = a.textContent || a.innerText;
+    //   if (txtValue.toUpperCase().indexOf(filter) > -1) {
+    //     li[i].style.display = "";
+    //   }
+
+    //   else {
+    //     li[i].style.display = "none";
+    //   }
+  }
+}
