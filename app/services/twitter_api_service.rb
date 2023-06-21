c03b734c35737b2555b593b257524b689335a04d
Alex Rich <alexanderrich28@gmail.com>
2023-06-21 22:44:25 +0100

    The Twitter API has been temporarily commented out in order to fix the app. Twitter API currently not working due to them now switching to a pay structure. Static comments have been included in place on the dynamic Twitter trends.

--- app/services/twitter_api_service.rb
+++ app/services/twitter_api_service.rb
@@ -1,32 +1,13 @@
 require "json"
 require "open-uri"
 
-class TwitterApiService
-  # def initialize
+# class TwitterApiService
+  # def client
+    # Twitter::REST::Client.new do |config|
+      # config.consumer_key        = ENV["CONSUMER_KEY"]
+      # config.consumer_secret     = ENV["CONSUMER_SECRET"]
+      # config.access_token        = ENV["ACCESS_TOKEN"]
+      # config.access_token_secret = ENV["ACCESS_SECRET"]
+    # end
   # end
-
-  # def globalcall
-  #   global_ideas = []
-  #   world_trend_url = "https://api.twitter.com/1.1/trends/place.json?id=1?exclude=hashtags"
-  #   world_trend_serialized = URI.open(world_trend_url).read
-  #   world_trends = JSON.parse(world_trend_serialized)
-  #   global_ideas << world_trends.first(5)
-  # end
-
-  # def localcall
-  #   local_ideas = []
-  #   local_trend_url = "https://api.twitter.com/1.1/trends/place.json?id=56000398?exclude=hashtags"
-  #   local_trend_serialized = URI.open(local_trend_url).read
-  #   local_trends = JSON.parse(local_trend_serialized)
-  #   local_ideas << local_trends.first(5)
-  # end
-
-  def client
-    Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV["CONSUMER_KEY"]
-      config.consumer_secret     = ENV["CONSUMER_SECRET"]
-      config.access_token        = ENV["ACCESS_TOKEN"]
-      config.access_token_secret = ENV["ACCESS_SECRET"]
-    end
-  end
-end
+# end
